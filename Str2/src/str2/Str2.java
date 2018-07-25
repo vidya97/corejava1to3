461fea14de48678de8aed774db48a39e753c5635
vidya97 <40888018+vidya97@users.noreply.github.com>
2018-07-25 16:53:58 +0530

    Add files via upload

---
+++ Str2/src/str2/Str2.java
@@ -0,0 +1,12 @@
+package str2;
+
+public class Str2 {
+
+    public static void main(String[] args) {
+        StringBuffer sb=new StringBuffer("Welocme ");
+        sb.append("VIDYA");
+        System.out.println(sb);
+        
+    }
+    
+}
